0eaa5ec10bf6c4bfc9c15c4320961b2f6040c0b9
tomoverman <tomovermanmhs@gmail.com>
2022-01-17 16:44:16 -0600

    work on general AR

---
+++ main.py
@@ -0,0 +1,4 @@
+from models import AR
+
+ar1 = AR(1)
+print(ar1.order)
--- models.py
+++ models.py
@@ -1,1 +1,25 @@
+import numpy as np
+class AR():
+    def __init__(self,order,num_vars,coeffs,std_devs,init_vals,N):
+        # if order is a constant
+        self.order=order
+        self.num_vars=num_vars
+        self.coeffs = coeffs
+        self.std_devs = std_devs
+        self.init_vals = init_vals
+        self.N = N
+
+    #generate the data using the AR models with the specified parameters
+    def generate_data(self):
+        data = np.zeros(num_vars,N)
+        data[:,0] = self.init_vals
+        for t in range(1,N):
+            data_mat = np.zeros(order*)
+            data[t,:] = np.matmul(coeffs,data_mat)
+
+        return data
+
+    # return mean and std dev of generated data
+    def get_stats(self):
+        pass
 
