1f70b9fb531d76b3cbe9cae92b6943b30e264871
Bennet <bennets@umich.edu>
2022-01-20 15:23:47 -0600

    Added another granger test

---
+++ granger_tests.py
@@ -0,0 +1,61 @@
+from models import AR
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def strong_and_weak_force_test(N):
+	np.random.seed(0)
+	# create inputs for 1st order with 3 variables
+	num_vars = 3
+	order = 1
+	# 2nd variable strongly forces 1st variable and 3rd variable weakly forces 1st variable
+	# first column corresponds to the constant terms
+
+	# variable index          0    1    2
+	# lag                     1    1    1
+	coeffs =  np.array([[1,   0,   100, 0.5], #eqn for X_0
+						[1,   0,   0,   0],   #eqn for X_1
+						[1,   0,   0,   0]])  #eqn for X_2
+
+	std_devs = np.random.uniform(.1,.3,num_vars)
+	init_vals = np.random.uniform(-1,1,(num_vars, order))
+	# create AR model object
+	ar = AR(num_vars,coeffs,std_devs,init_vals)
+	# generate data using model
+	ar.generate_data(N)
+	ar.causality(order,0.05)
+
+	# plot data
+	for i in range(num_vars):
+		plt.plot(ar.data[i], label="$X_{:d}$".format(i))
+	plt.legend()
+	plt.show()
+
+def basic_test():
+	np.random.seed(0)
+	# create inputs for 2nd order with 4 variables
+	num_vars = 4
+	order = 2
+
+	# first column corresponds to the constant terms
+	# variable index          0   0   1   1   2   2   3   3
+	# lag                     2   1   2   1   2   1   2   1
+	coeffs =  np.array([[40,  1, -3,  1, -2,  1,  3,  0,  0],   #eqn for variable 0
+						[30,  0,  2,  0,  0,  0,  0,  0,  0],   #eqn for variable 1
+						[10,  0,  0,  0,  0, -3,  1,  0,  0],   #eqn for variable 2 
+						[0,   0,  0,  0,  0,  0,  0,  0,  0],]) #eqn for variable 3
+
+	std_devs = np.array([0.1, 0.1, 0.1, .1])
+	init_vals = np.random.uniform(-1,1,(num_vars, order))
+	N=10000
+	# create AR model object
+	ar = AR(num_vars,coeffs/10,std_devs,init_vals)
+	# generate data using model
+	ar.generate_data(N)
+	ar.causality(order,0.05)
+
+	# plot data
+	for i in range(num_vars):
+		plt.plot(ar.data[i], label="$X_{:d}$".format(i))
+	plt.legend()
+	plt.show()
--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from models import AR
-import numpy as np
-import matplotlib.pyplot as plt
+from granger_tests import *
 
-# create inputs for 1st order with 3 variables
-# 2nd variable strongly forces 1st variable and 3rd variable weakly forces 1st variable
-# first column corresponds to the constant terms
-coeffs = np.array([[1,0,100,1],[1,0,0,0],[1,0,0,0]])
-print(coeffs.shape)
-std_devs = np.random.uniform(.1,.3,3)
-init_vals = np.random.uniform(-1,1,(3,1))
-N=10
-# create AR model object
-ar = AR(3,coeffs,std_devs,init_vals)
-# generate data using model
-ar.generate_data(N)
-order=1
-ar.causality(order,0.05)
-
-# plot data
-plt.plot(ar.data[0])
-plt.plot(ar.data[1])
-plt.plot(ar.data[2])
-plt.show()
+strong_and_weak_force_test(N=10)
+strong_and_weak_force_test(N=100)
+basic_test()
